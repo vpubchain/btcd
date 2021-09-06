659cd4859672012039de97a6d97eca67c0e45184
jagdeep sidhu <jsidhu@blockchainfoundry.co>
2021-09-05 22:28:01 -0700

    Update msgtxsyscoin.go

--- wire/msgtxsyscoin.go
+++ wire/msgtxsyscoin.go
@@ -15,7 +15,7 @@ const (
 	MAX_SIG_SIZE = 65
 	MAX_RLP_SIZE = 4096
 	HASH_SIZE = 32
-	MAX_NEVM_BLOCK_SIZE = (32 << 20) // 32 MiB
+	MAX_NEVM_BLOCK_SIZE = (1024 << 20) // 1024 MiB
 )
 const ( 	
 	ASSET_UPDATE_DATA = 1 // can you update public data field?
